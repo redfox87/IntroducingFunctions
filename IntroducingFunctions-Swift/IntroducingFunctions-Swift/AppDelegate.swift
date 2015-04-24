1c119a8331a0dfbfea2ab7ef3354eecf3aa64e0c
Thomas Bloss <runredfox@gmail.com>
2015-04-24 15:22:38 -0400

    coded

--- IntroducingFunctions-Swift/IntroducingFunctions-Swift/AppDelegate.swift
+++ IntroducingFunctions-Swift/IntroducingFunctions-Swift/AppDelegate.swift
@@ -18,12 +18,30 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         // Insert code here...
         
+        let myName: String = "Thomas Bloss"
+        
+        countDownTillIntroduction(4)
+        printIntroductions(myName)
         
         return true
     }
-
+    
     // Define new functions here...
-
+    
+    func printIntroductions(name: String) {
+        let introduction: String = "My name is " + name
+        let japaneseIntroduction: String = introduction + " to moushimasu"
+        NSLog("Introduction: \"\(introduction)\"")
+        NSLog("Japanese introduction: \"\(japaneseIntroduction)\"")
+    }
+    
+    func countDownTillIntroduction(numberOfDays: Int) {
+        if (numberOfDays == 0) {
+            NSLog("The time has come")
+        } else {
+            NSLog("\(numberOfDays) days left until introductions")
+            countDownTillIntroduction(numberOfDays-1)
+        }
+    }
     
 }
-
